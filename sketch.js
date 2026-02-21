125c72244878fff60e8cb2a86b30265467698cd9
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-21 00:26:02 -0500

    refactor eye drawing into class

--- sketch.js
+++ sketch.js
@@ -1,74 +1,80 @@
+function draw() {}
+
+class Eye {
+  constructor(eyeSide) {
+    let side = eyeSide === "right" ? -1 : 1;
+    // calculate dimensions for each eye
+    //
+    //       ┌─────────────────────────── eyelidPeak
+    //       │
+    //       ▼          ├──────────┤◄──── palpebralFissureX
+    // ⢀⣀⣤⣤⡖⠛⣭⡉⠓⣦⡀⠀├───┤⠀⠀⣠⢴⠒⠒⠒⠦⢄⡀⠀⠀┬
+    // ⠙⣇⠀⠀⣷⡀⠛⠃⢀⡇⠙⢦ ⠀▲⠀⢀⡿⠟⠉⢿⠀⠿⠃⢀⡇⠈⣱⣦│◄─── palpebralFissureY
+    // ⠀⠈⠳⣄⠀⠙⠓⠋⠉⣀⡔⠋⠀⠀│⠀⠀⠻⣦⡀⠘⠷⠦⠶⠋⢀⡴⠋⠀│
+    // ⠀⠀⠀⠈⠓⠦⠤⠖⠚⠁⠀⠀⠀⠀│⠀⠀⠀⠀⠙⠷⣦⣤⣶⠞⠉⠀⠀⠀┴
+    //               └─────────────────── innerCanthalDistance
+
+    const innerCanthalDistance = width / 10;
+    const centerOffset = innerCanthalDistance / 2;
+
+    const palpebralFissureX = width * 0.37;
+    const palpebralFissureY = width / 6;
+    const yOffset = palpebralFissureY / 2;
+
+    const eyelidPeak = palpebralFissureX * 0.48 + centerOffset;
+
+    // calculate positions for each eye
+    //
+    // ┌───────── outer ──────────┐
+    // │     ┌─── top ──────┐     │
+    // │     ▼              ▼     │
+    // ▼⣀⣤⣤⡖⠛⣭⡉⠓⣦⡀⠀⠀⠀⠀⠀⠀⠀⣠⢴⠒⠒⠒⠦⢄⡀⠀▼
+    // ⠙⣇⠀⠀⣷⡀⠛⠃⢀⡇⠙⢦⠀⠀⠀⢀⡿⠟⠉⢿⠀⠿⠃⢀⡇⠈⣱⣦
+    // ⠀⠈⠳⣄⠀⠙⠓⠋⠉⣀⡔⠋⠀⠀⠀⠀⠻⣦⡀⠘⠷⠦⠶⠋⢀⡴⠋⠀
+    // ⠀⠀⠀⠈⠓⠦⠤⠖⠚⠁⠀▲⠀ ⠀▲⠀⠀⠙⠷⣦⣤⣶⠞⠉⠀⠀⠀
+    //       ▲    └─┬─┘     ▲
+    //       │    inner     │
+    //       └─── bottom ───┘
+
+    // note: sides are based off of the EYES' POV
+    //   RIGHT              LEFT
+
+    this.inner = {
+      x: side * centerOffset,
+      y: 0,
+    };
+    this.top = {
+      x: side * eyelidPeak,
+      y: yOffset,
+    };
+    this.outer = {
+      x: side * (centerOffset + palpebralFissureX),
+      y: 0,
+    };
+    this.bottom = {
+      x: side * eyelidPeak,
+      y: -1 * yOffset,
+    };
+  }
+
+  see() {
+    push();
+    translate(width / 2, height / 2);
+    beginShape();
+    splineVertex(this.inner.x, this.inner.y);
+    splineVertex(this.top.x, this.top.y);
+    splineVertex(this.outer.x, this.outer.y);
+    splineVertex(this.bottom.x, this.bottom.y);
+    endShape(CLOSE);
+    pop();
+  }
+}
+
 function setup() {
   createCanvas(600, 600);
   background(255);
-  drawEye("right");
-  drawEye("left");
-}
-
-function draw() {}
-
-function drawEye(side) {
-  // draw eyes based on center
-  translate(width / 2, height / 2);
-
-  // calculate dimensions for each eye
-  //
-  //       ┌─────────────────────────── eyelidPeak
-  //       │
-  //       ▼          ├──────────┤◄──── palpebralFissureX
-  // ⢀⣀⣤⣤⡖⠛⣭⡉⠓⣦⡀⠀├───┤⠀⠀⣠⢴⠒⠒⠒⠦⢄⡀⠀⠀┬
-  // ⠙⣇⠀⠀⣷⡀⠛⠃⢀⡇⠙⢦ ⠀▲⠀⢀⡿⠟⠉⢿⠀⠿⠃⢀⡇⠈⣱⣦│◄─── palpebralFissureY
-  // ⠀⠈⠳⣄⠀⠙⠓⠋⠉⣀⡔⠋⠀⠀│⠀⠀⠻⣦⡀⠘⠷⠦⠶⠋⢀⡴⠋⠀│
-  // ⠀⠀⠀⠈⠓⠦⠤⠖⠚⠁⠀⠀⠀⠀│⠀⠀⠀⠀⠙⠷⣦⣤⣶⠞⠉⠀⠀⠀┴
-  //               └─────────────────── innerCanthalDistance
-
-  const innerCanthalDistance = width / 10;
-  const centerOffset = innerCanthalDistance / 2;
-
-  const palpebralFissureX = width * 0.37;
-  const palpebralFissureY = width / 6;
-  const yOffset = palpebralFissureY / 2;
-  
-	const eyelidPeak = palpebralFissureX * 0.48 + centerOffset;
-
-  // calculate positions for each eye
-  //
-  // ┌───────── outer ──────────┐
-  // │     ┌─── top ──────┐     │
-  // │     ▼              ▼     │
-  // ▼⣀⣤⣤⡖⠛⣭⡉⠓⣦⡀⠀⠀⠀⠀⠀⠀⠀⣠⢴⠒⠒⠒⠦⢄⡀⠀▼
-  // ⠙⣇⠀⠀⣷⡀⠛⠃⢀⡇⠙⢦⠀⠀⠀⢀⡿⠟⠉⢿⠀⠿⠃⢀⡇⠈⣱⣦
-  // ⠀⠈⠳⣄⠀⠙⠓⠋⠉⣀⡔⠋⠀⠀⠀⠀⠻⣦⡀⠘⠷⠦⠶⠋⢀⡴⠋⠀
-  // ⠀⠀⠀⠈⠓⠦⠤⠖⠚⠁⠀▲⠀ ⠀▲⠀⠀⠙⠷⣦⣤⣶⠞⠉⠀⠀⠀
-  //       ▲    └─┬─┘     ▲
-  //       │    inner     │
-  //       └─── bottom ───┘
-
-  // note: sides are based off of the EYES' POV
-  //   RIGHT              LEFT
-
-  const leftEye = {
-    inner: {
-      x: centerOffset,
-      y: 0,
-    },
-    top: {
-      x: eyelidPeak,
-      y: yOffset,
-    },
-    outer: {
-      x: centerOffset + palpebralFissureX,
-      y: 0,
-    },
-		bottom: {
-			x: eyelidPeak,
-			y: -1 * yOffset,
-		}
-  };
-  beginShape();
-  splineVertex(leftEye.inner.x, leftEye.inner.y);
-  splineVertex(leftEye.top.x, leftEye.top.y);
-  splineVertex(leftEye.outer.x, leftEye.outer.y);
-  splineVertex(leftEye.bottom.x, leftEye.bottom.y);
-  endShape(CLOSE);
+  let leftEye = new Eye("left");
+  let rightEye = new Eye("right");
+  leftEye.see();
+  rightEye.see();
 }
